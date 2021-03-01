57815e6fcf0a54d2f869564ec88348a6199dca95
Anand Kane <anand@infracloud.io>
2021-03-01 18:55:35 +0530

    Commented out //@EnableWebMvcSecurity

--- src/main/java/com/example/contactbook/SecurityConfig.java
+++ src/main/java/com/example/contactbook/SecurityConfig.java
@@ -3,10 +3,9 @@ package com.example.contactbook;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-import org.springframework.security.config.annotation.web.servlet.configuration.EnableWebMvcSecurity;
 
 @Configuration
-@EnableWebMvcSecurity
+//@EnableWebMvcSecurity
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
 	@Override
 	protected void configure(HttpSecurity http) throws Exception {
